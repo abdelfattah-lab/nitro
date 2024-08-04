87de22d3f003a85861a2dd3e4a042377e39c03d2
Anthony Fei <anthonyf1223@gmail.com>
2024-08-04 13:32:11 -0400

    Finished conversion module + verification on Llama - input_generators file for generating example inputs - chunk conversion for PyTorch -> OpenVINO

--- converter/converter.py
+++ converter/converter.py
@@ -7,6 +7,12 @@ from transformers import AutoModelForCausalLM
 from pathlib import Path
 
 from converter.bindings import get_args, get_model
+from converter.input_generators import generate_auto, generate_shape
+from converter.chunk_conversion import conversion_wrapper
+
+import openvino_tokenizers as ot
+from transformers import AutoTokenizer
+
 import os
 
 class Converter():
@@ -24,11 +30,22 @@ class Converter():
         """
         self.model_name = model
 
+        # Configuring directories
         if directory == None: directory = "ir_model"
+        
         self.directory = Path(directory)
         if not os.path.isdir(self.directory):
             os.makedirs(self.directory)
+        
+        self.llm_directory = self.directory / "model"
+        if not os.path.isdir(self.llm_directory):
+            os.makedirs(self.llm_directory)
 
+        self.tokenizer_directory = self.directory / "tokenizer"
+        if not os.path.isdir(self.tokenizer_directory):
+            os.makedirs(self.tokenizer_directory)
+
+    def initialize_model(self, args:Any=None):
         # Saves and loads the weights
         self._save_weights()
 
@@ -37,25 +54,52 @@ class Converter():
         self.model_args = get_args(self.model_name)
 
         self.pytorch_model.load_state_dict(torch.load(self.directory / "model_weights.pth"))
-
+    
+    def convert(self):
         # Generates example inputs for conversion
-        self._generate_example_inputs_for_conversion()
+        example_inputs = generate_auto(self.model_args, "x", "mask", "freqs_cis", "kv_caches")
+        shapes = generate_shape(example_inputs)
 
         # Conversion to IR Format / Chunking
-        self.convert()
+        self._convert_chunks(example_inputs, shapes)
+
+        self.generate_tokenizers()
 
 
-    def convert(self) -> ov.Model:
+    def _convert_chunks(self, example_inputs, shapes) -> None:
         """
         Converts the PyTorch LLM model into chunks.
         """
+        count = 0
+        print("Converting embedding layer...")
+        self.pytorch_model.include_embedding, self.pytorch_model.include_transformer, self.pytorch_model.include_output = True, False, False
+        conversion_wrapper(self.pytorch_model, count, self.llm_directory, example_inputs, shapes)
+        count += 1
+
+        ############ Chunking transformer layers ############
+        print("Converting transformer layers...")
+        self.pytorch_model.include_embedding, self.pytorch_model.include_transformer, self.pytorch_model.include_output = False, True, False
+
+        for offset in range(0, self.model_args.n_layers, self.model_args.chunk_size):
+            print(f" > Block: {offset}-{offset + self.model_args.chunk_size-1}")
+            self.pytorch_model.offset = offset
+
+            conversion_wrapper(self.pytorch_model, count, self.llm_directory, example_inputs, shapes)
+            count += 1
+
+        ############ Chunking output layer ############
+        self.pytorch_model.include_embedding, self.pytorch_model.include_transformer, self.pytorch_model.include_output = False, False, True
+        print("Converting output layer...")
+
+        conversion_wrapper(self.pytorch_model, count, self.llm_directory, example_inputs, shapes)
+        count += 1
         
-        
-    def _generate_example_inputs_for_conversion(self):
-        pass
 
 
     def _save_weights(self) -> None:
+        """
+        Obtains the weights from Hugging Face.
+        """
         if not os.path.isfile(os.path.join(self.directory, "model_weights.pth")):
             print("Weights not found, loading model from Hugging Face:")
             model = AutoModelForCausalLM.from_pretrained(self.model_name)
@@ -75,24 +119,12 @@ class Converter():
             del model_weights
             del model
 
-
-    def _stateful_model(self, suffix_match:str="_out") -> ov.Model:
-        """
-        Converts the models into stateful models. Stateful models are created by
-        matching parameter names [x] with result names [x{suffix_match}]. The
-        shapes must be the same.
-        """
-        pass
-
-
-
-    def _rename(self):
-        pass
-
     def generate_tokenizers(self):
         """
-        Generates.
+        Generates tokenizers.
         """
-        pass
-
-Converter("meta-llama/Meta-Llama-3-8B", "npu_model")
+        print("Generating tokenizers...")
+        hf_tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+        ov_tokenizer, ov_detokenizer = ot.convert_tokenizer(hf_tokenizer, with_detokenizer=True, skip_special_tokens=True)
+        ov.save_model(ov_tokenizer, self.tokenizer_directory / "openvino_tokenizer.xml")
+        ov.save_model(ov_detokenizer, self.tokenizer_directory / "openvino_detokenizer.xml")
---
+++ converter/input_generators.py
@@ -0,0 +1,82 @@
+"""
+In conversion of PyTorch models to OpenVINO, a critical component is providing
+example inputs in the convert_model function. When the model graph is complex
+(which is often the case LLMs), scripting may require an example_input.
+
+This file provides helper functions to generate example_inputs.
+"""
+import torch
+from pytorch_model.utils.model_utils import precompute_freqs_cis_rect
+from typing import Any
+
+def generate_x(args) -> torch.Tensor:
+    return torch.randint(0, args.vocab_size, [args.max_batch_size, args.inference_size])
+
+def generate_mask(args) -> torch.Tensor:
+    return torch.full([args.max_batch_size, args.n_heads, args.inference_size, args.max_seq_len], float('-inf'))
+
+def generate_freq_cis(args) -> torch.Tensor:
+    return precompute_freqs_cis_rect(
+                args.dim // args.n_heads,
+                args.max_seq_len * 2,
+                args.rope_theta
+            )[0:args.inference_size]
+
+def generate_kv_caches(args) -> dict[str, torch.Tensor]:
+    params = {}
+    for i in range(args.n_layers):
+        params[f"cache_k_{i}"] = torch.zeros([args.max_batch_size, args.max_seq_len, args.n_kv_heads, args.dim // args.n_heads])
+        params[f"cache_v_{i}"] = torch.zeros([args.max_batch_size, args.max_seq_len, args.n_kv_heads, args.dim // args.n_heads])
+    return params
+
+def generate_auto(args, *input_names:str) -> dict[str, torch.Tensor | dict[str, torch.Tensor]]:
+    inputs = {}
+
+    mapping = {
+        "x" : generate_x,
+        "mask" : generate_mask,
+        "freqs_cis" : generate_freq_cis,
+        "kv_caches" : generate_kv_caches,
+    }
+
+    for input_name in input_names:
+        if input_name not in mapping:
+            raise ValueError(f"{input_name} is not a valid input name!")
+        func = mapping[input_name]
+        inputs[input_name] = func(args)
+    
+    return inputs
+
+def flatten_dict(d):
+    def recurse(t, parent_key=''):
+        items = {}
+        for k, v in t.items():
+            new_key = k  # Use the current key only
+            if isinstance(v, dict):
+                nested_items = recurse(v, new_key)
+                for nk, nv in nested_items.items():
+                    if nk in items:
+                        raise KeyError(f"Key conflict: {nk}")
+                    items[nk] = nv
+            else:
+                if new_key in items:
+                    raise KeyError(f"Key conflict: {new_key}")
+                items[new_key] = v
+        return items
+
+    return recurse(d)
+
+def generate_shape_aux(inputs) -> dict[str, Any]:
+    if isinstance(inputs, dict):
+        shape_dict = {}
+        for key, value in inputs.items():
+            shape_dict[key] = generate_shape_aux(value)
+        return shape_dict
+    elif isinstance(inputs, torch.Tensor):
+        return inputs.shape
+    else:
+        raise TypeError("Unsupported type: {}".format(type(inputs)))
+    
+def generate_shape(inputs) -> dict[str, torch.Size]:
+    nested_shapes = generate_shape_aux(inputs)
+    return flatten_dict(nested_shapes) # flattens the nested-kv_caches dict.
